249edf43cae017dba37385555ff90debe13e2d28
Brandon Jackson <brandonljackson23@gmail.com>
2020-05-16 19:09:57 -0600

    refactor entire pwd generator

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,9 @@
 // Assignment code here
+var yes = 0;
 
-var lowercaseMin = 0;
-var lowercaseMax = 0;
-var uppercaseMin = 0;
-var lowercaseMax = 0;
-var specialMin = 0;
-var uppercaseMax = 0;
-var numberMin = 0;
-var numberMax = 0;
+var pwdLength = prompt("How long do you want the password to be?  Enter the number of characters you want (from 8-128.)");
 
-var passLength = prompt("How long do you want the password to be?  Enter the number of characters you want (from 8-128.)");
-
-if (passLength >= 8 && passLength <= 128) {
+if (pwdLength >= 8 && pwdLength <= 128) {
 } 
 else {
   alert("Please enter a number between 8 and 128.");
@@ -21,103 +13,71 @@ var includeLowercase = prompt("Do you want to include lowercase letters? (y/n)")
   includeLowercase = includeLowercase.toLowerCase();
 
 if (includeLowercase === 'y') {
-
-  lowercaseMin = 97;
-  lowercaseMax = 122;
-
+  yes++;
 } else if (includeLowercase === 'n') {
-    
-  lowercaseMin = 127;
-  lowercaseMax = 0;
-  
 } else {
-    alert("Please enter either 'y' or 'n'.");
-}    
+  alert("Please enter either 'y' or 'n'.");
+}   
 
 var includeUppercase = prompt("Do you want to include capital letters? (y/n)");
     includeUppercase = includeUppercase.toLowerCase();
 
 if (includeUppercase === 'y') {
-
-  uppercaseMin = 65;
-  uppercaseMax = 90;
-
+  yes++;
 } else if (includeUppercase === 'n') {
-
-  uppercaseMin = 127;
-  uppercaseMax = 0;
-
 } else {
-alert("Please enter either 'y' or 'n'.");
-}
+  alert("Please enter either 'y' or 'n'.");
+}  
 
+var includeNumber = prompt("Wo you want to include numbers? (y/n)");
+  includeNumber = includeNumber.toLowerCase();
+
+if (includeNumber === 'y') {
+  yes++;
+} else if (includeNumber === 'n') {
+} else {
+  alert("Please enter either 'y' or 'n'.");
+}  
 
 var includeSpecial = prompt("Do you want to include special characters? (y/n)");
 includeSpecial = includeSpecial.toLowerCase();
 
 if (includeSpecial === 'y') {
-
-  specialMin = 58;
-  specialMax = 126;
-
+  yes++;
 } else if (includeSpecial === 'n') {
-
-  specialMin = 127;
-  specialMax = 0;
-
 } else {
   alert("Please enter either 'y' or 'n'.");
 }
- 
-var includeNumber = prompt("Wo you want to include numbers? (y/n)");
-  includeNumber = includeNumber.toLowerCase();
 
-if (includeNumber === 'y') {
+function makePwd(pwdLength) {
+    var result = '';
+    var lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+    var lowercaseCharsLength = lowercaseChars.length;
+    var uppercaseChars = lowercaseChars.toUpperCase();
+    var uppercaseCharsLength = uppercaseChars.length;
+    var numberChars = '0123456789'
+    var numberCharsLength = numberChars.length;
+    var specialChars = '`~!@#$%^&*()-_=+[{]}\|;:,<.>/?';
+    var specialCharsLength = specialChars.length;
 
-  numberMin = 48;
-  numberMax = 57;
-
-} else if (includeNumber === 'n') {
-  
-  numberMin = 127;
-  numberMax = 0;
-
-} else {
-  alert("Please enter either 'y' or 'n'.");
-}
-      
-
-    
-  
-
-  function generate_random_string(string_length){
-    let random_string = '';
-    let random_ascii;
-    let ascii_low = 48;
-    let ascii_high = 126;
-    for(let i = 0; i < string_length; i++) {
-        random_ascii = Math.floor((Math.random() * (ascii_high - ascii_low)) + ascii_low);
-        random_string += String.fromCharCode(random_ascii)
+    for (var i =0; i < pwdLength; i++) {
+      if (includeLowercase === 'y') {
+        result += lowercaseChars.charAt(Math.floor(Math.random() * lowercaseCharsLength));
+      }
+      if (includeUppercase === 'y') {
+        result += uppercaseChars.charAt(Math.floor(Math.random() * uppercaseCharsLength));
+      }
+      if (includeNumber === 'y') {
+        result += numberChars.charAt(Math.floor(Math.random() * numberCharsLength));
+      }
+      if (includeSpecial === 'y') {
+        result += specialChars.charAt(Math.floor(Math.random() * specialCharsLength));
+      }
     }
-    return random_string
-  }
+    return result;
+}
 
-  function generate() {
-    return generate_random_string(passLength)
-  }
-
-  console.log("password: " + generate())
-  console.log("lowercaseMin: " + lowercaseMin)
-  console.log("lowercaseMax: " + lowercaseMax)
-  console.log("uppercaseMin: " + uppercaseMin)
-  console.log("uppercaseMax: " + uppercaseMax)
-  console.log("specialMin: " + specialMin)
-  console.log("specialMax: " + specialMax)
-  console.log("numberMin: " + numberMin)
-  console.log("numberMax: " + numberMax)
-
-
-
+console.log(makePwd(pwdLength / yes).substring(0, pwdLength));
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
